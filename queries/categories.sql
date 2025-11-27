efe0023e6e8b4568f3f61dc575d13c76cde53a6c
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2025-11-27 11:25:54 +0530

    Query valid categories - exclude hidden

--- queries/categories.sql
+++ queries/categories.sql
@@ -1,1 +1,8 @@
-SELECT page_id, page_title FROM page WHERE page_namespace = 14
+SELECT p.page_id, p.page_title
+FROM page p
+WHERE p.page_namespace = 14  -- 14 is the namespace for categories
+AND p.page_id NOT IN (
+    SELECT pp_page 
+    FROM page_props 
+    WHERE pp_propname = 'hiddencat'
+);
