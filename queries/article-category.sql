e3ae31812856d1affae9176c7d1678af2b452cbb
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2025-11-28 22:32:24 +0530

    query optimization

--- queries/article-category.sql
+++ queries/article-category.sql
@@ -2,8 +2,7 @@ SELECT cl.cl_from AS article_id,
        p.page_id AS category_page_id 
 FROM categorylinks cl 
 JOIN linktarget lt ON lt.lt_id = cl.cl_target_id
-JOIN page p ON lt.lt_title = p.page_title 
-             AND p.page_namespace = 14
-WHERE lt.lt_namespace = 14 
-  AND cl.cl_type = 'page'
+JOIN page p ON lt.lt_title = p.page_title AND lt.lt_namespace = 14 AND p.page_namespace = 14
+WHERE lt.lt_namespace = 14 AND cl.cl_type = 'page'
+
 
